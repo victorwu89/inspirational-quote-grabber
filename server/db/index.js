c13867c26d569b82ea8f468d30d0e0ce5fd0abf0
Victor Wu <victorwu89@gmail.com>
2020-02-11 11:17:49 +0100

    Update index.js
    removed  direct info to mongo atlas

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 //TODO Add environment variable so password and url is protected
 mongoose
-  .connect('mongodb+srv://victorwu:password89@cluster0-1j562.mongodb.net/quotes_Table?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true})
+  .connect('' , { useNewUrlParser: true, useUnifiedTopology: true})
   .catch(e => {
     console.error('Connection error', e.message)
   })
